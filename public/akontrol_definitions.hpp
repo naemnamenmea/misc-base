f9ac942293c4b4a078ddde4405dd7261556649a2
naemnamenmea <kontrolskiy@mail.ru>
2025-02-03 21:26:54 +0300

    minor changes

--- public/akontrol_definitions.hpp
+++ public/akontrol_definitions.hpp
@@ -2,8 +2,8 @@
 
 #ifndef GCC_COMPILER
 #if (defined(__GNUC__) || defined(__GNUG__)) && not defined(__clang__)
-#define GCC_COMPILER 1
+#define GCC_COMPILER 1 // HAS_GNU
 #else
-#define GCC_COMPILER 0
+#define GCC_COMPILER 0 // HAS_GNU
 #endif
 #endif
